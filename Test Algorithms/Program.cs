5550c13caf3b81adaafc099c956104a6e9502932
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-06-04 11:05:26 +0330

    .

--- Test Algorithms/Program.cs
+++ Test Algorithms/Program.cs
@@ -12,7 +12,7 @@ int[] array1 = { 38, 27, 43, 3, 9, 82, 10 };
 ISortable<int> mergesort = new MergeSort<int>();
 mergesort.Sort(array1);
 
-Console.WriteLine("\nMergeSort\nSorted array:");
+Console.WriteLine("\n1-MergeSort\nSorted array:");
 foreach (var item in array1)
 {
     Console.Write(item + " ");
@@ -24,7 +24,7 @@ int[] array2 = { 12, 11, 13, 5, 6, 7 };
 ISortable<int> heapsort = new HeapSort<int>();
 heapsort.Sort(array2);
 
-Console.WriteLine("HeapSort\nSorted array:");
+Console.WriteLine("2-HeapSort\nSorted array:");
 foreach (var item in array2)
 {
     Console.Write(item + " ");
@@ -36,7 +36,7 @@ int[] array3 = { 12, 34, 54, 2, 3 };
 ISortable<int> shellsort = new ShellSort<int>();
 shellsort.Sort(array3);
 
-Console.WriteLine("ShellSort\nSorted array:");
+Console.WriteLine("3-ShellSort\nSorted array:");
 foreach (var item in array3)
 {
     Console.Write(item + " ");
@@ -48,7 +48,7 @@ int[] array4 = { 64, 25, 12, 22, 11 };
 ISortable<int> selectionsort = new SelectionSort<int>();
 selectionsort.Sort(array4);
 
-Console.WriteLine("SelectionSort\nSorted array:");
+Console.WriteLine("4-SelectionSort\nSorted array:");
 foreach (var item in array4)
 {
     Console.Write(item + " ");
@@ -60,7 +60,7 @@ int[] array5 = { 12, 11, 13, 5, 6 };
 ISortable<int> insertionsort = new InsertionSort<int>();
 insertionsort.Sort(array5);
 
-Console.WriteLine("InsertionSort\nSorted array:");
+Console.WriteLine("5-InsertionSort\nSorted array:");
 foreach (var item in array5)
 {
     Console.Write(item + " ");
@@ -72,7 +72,7 @@ int[] array6 = { 12, 11, 13, 5, 6, 6546, 312, 1, 647, 121 };
 ISortable<int> bubblesort = new BubbleSort<int>();
 bubblesort.Sort(array6);
 
-Console.WriteLine("BubbleSort\nSorted array:");
+Console.WriteLine("6-BubbleSort\nSorted array:");
 foreach (var item in array6)
 {
     Console.Write(item + " ");
@@ -84,7 +84,7 @@ int[] array7 = { 102, 8, 1221, 5, 6, 44, 312, 1, 564, 97, 83274 };
 ISortable<int> quicksort = new QuickSort<int>();
 quicksort.Sort(array7);
 
-Console.WriteLine("QuickSort\nSorted array:");
+Console.WriteLine("7-QuickSort\nSorted array:");
 foreach (var item in array7)
 {
     Console.Write(item + " ");
