89147b645bafec1bf0e3876367d78b204af4df36
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-06-04 09:56:48 +0330

    add new algorithms

---
+++ Insertion.Implementations/InsertionSort.cs
@@ -0,0 +1,23 @@
+﻿using InterfaceSort;
+
+namespace Insertion.Implementations
+{
+    public class InsertionSort<T> : ISortable<T> where T : IComparable<T>
+    {
+        public void Sort(T[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                T key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j].CompareTo(key) > 0)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+        }
+    }
+}
---
+++ Selection.Implementations/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using InterfaceSort;
+
+namespace Selection.Implementations
+{
+    public class SelectionSort<T> : ISortable<T> where T : IComparable<T>
+    {
+        public void Sort(T[] array)
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Find the minimum element in unsorted array
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j].CompareTo(array[minIndex]) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                // Swap the found minimum element with the first element
+                T temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
