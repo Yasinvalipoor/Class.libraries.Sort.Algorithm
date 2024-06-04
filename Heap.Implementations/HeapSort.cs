6556e6599634283c59a73c73b5e654b235127c5f
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-06-04 10:06:20 +0330

    Merge Sort - Heap Sort

---
+++ Heap.Implementations/HeapSort.cs
@@ -0,0 +1,46 @@
+﻿using InterfaceSort;
+
+namespace Heap.Implementations
+{
+    public class HeapSort<T> : ISortable<T> where T : IComparable<T>
+    {
+        public void Sort(T[] array)
+        {
+            int n = array.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(array, n, i);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                T temp = array[0];
+                array[0] = array[i];
+                array[i] = temp;
+
+                Heapify(array, i, 0);
+            }
+        }
+
+        void Heapify(T[] array, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && array[left].CompareTo(array[largest]) > 0)
+                largest = left;
+
+            if (right < n && array[right].CompareTo(array[largest]) > 0)
+                largest = right;
+
+            if (largest != i)
+            {
+                T swap = array[i];
+                array[i] = array[largest];
+                array[largest] = swap;
+
+                Heapify(array, n, largest);
+            }
+        }
+    }
+}
---
+++ Merge.Implementations/MergeSort.cs
@@ -0,0 +1,61 @@
+﻿using InterfaceSort;
+
+namespace Merge.Implementations
+{
+    public class MergeSort<T> : ISortable<T> where T : IComparable<T>
+    {
+        public void Sort(T[] array)
+        {
+            if (array.Length <= 1)
+            {
+                return;
+            }
+
+            T[] tempArray = new T[array.Length];
+            MergeSortHelper(array, tempArray, 0, array.Length - 1);
+        }
+
+        private void MergeSortHelper(T[] array, T[] tempArray, int leftStart, int rightEnd)
+        {
+            if (leftStart >= rightEnd)
+            {
+                return;
+            }
+
+            int middle = (leftStart + rightEnd) / 2;
+            MergeSortHelper(array, tempArray, leftStart, middle);
+            MergeSortHelper(array, tempArray, middle + 1, rightEnd);
+            MergeHalves(array, tempArray, leftStart, rightEnd);
+        }
+
+        private void MergeHalves(T[] array, T[] tempArray, int leftStart, int rightEnd)
+        {
+            int leftEnd = (rightEnd + leftStart) / 2;
+            int rightStart = leftEnd + 1;
+            int size = rightEnd - leftStart + 1;
+
+            int left = leftStart;
+            int right = rightStart;
+            int index = leftStart;
+
+            while (left <= leftEnd && right <= rightEnd)
+            {
+                if (array[left].CompareTo(array[right]) <= 0)
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+                index++;
+            }
+
+            Array.Copy(array, left, tempArray, index, leftEnd - left + 1);
+            Array.Copy(array, right, tempArray, index, rightEnd - right + 1);
+            Array.Copy(tempArray, leftStart, array, leftStart, size);
+        }
+    }
+}
